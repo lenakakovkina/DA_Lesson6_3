bb2df54ac2faeebaed947b45deec03654399b33a
Samsung <elenakakovkina89@gmail.com>
2024-07-03 22:26:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int guestNumber = 5;
+int guestNumberInPlace = 0;
+do
+{
+    Console.WriteLine("Hi! What is your name?");
+    string UserName = Console.ReadLine();
+    Console.WriteLine($"Nice to meet you {UserName}");
+    guestNumberInPlace++;
+}
+while (guestNumberInPlace != guestNumber);
+Console.WriteLine("All guests are here. We can start!");
